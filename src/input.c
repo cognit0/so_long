6c8a6e94bc5622292f171d4edce4ffb5fc9bc7b8
Arda Yalman <ardayalman@192.168.1.100>
2022-04-02 00:00:00 +0300

    map

--- src/input.c
+++ src/input.c
@@ -30,21 +30,28 @@ int  ft_keyPressed(int keycode, t_mlx *handle)
 	{
 		handle->player_x+=32;
 		handle->number_of_moves++;
+		mlx_destroy_image(handle->mlx, handle->sprite_player);
+		handle->sprite_player = mlx_xpm_file_to_image(handle->mlx, "img/player_turn_right.xpm", &handle->sprite_w, &handle->sprite_h);
 	}
 	else if (keycode == KEY_W && collision(handle, KEY_W))
 	{
 		handle->player_y-=32;
 		handle->number_of_moves++;
+		mlx_destroy_image(handle->mlx, handle->sprite_player);
+		handle->sprite_player = mlx_xpm_file_to_image(handle->mlx, "img/player_turn_back.xpm", &handle->sprite_w, &handle->sprite_h);
 	}
 	else if (keycode == KEY_A && collision(handle, KEY_A))
 	{
 		handle->player_x-=32;
 		handle->number_of_moves++;
+		mlx_destroy_image(handle->mlx, handle->sprite_player);
+		handle->sprite_player = mlx_xpm_file_to_image(handle->mlx, "img/player_turn_left.xpm", &handle->sprite_w, &handle->sprite_h);
 	}
 	else if (keycode == KEY_S && collision(handle, KEY_S))
 	{
 		handle->player_y+=32;
 		handle->number_of_moves++;
+		handle->sprite_player = mlx_xpm_file_to_image(handle->mlx, "img/player_turn_front.xpm", &handle->sprite_w, &handle->sprite_h);
 	}
 	return (0);
 }
